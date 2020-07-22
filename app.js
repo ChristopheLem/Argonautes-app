c2bda70560add831673d0dc5711edb6c00413a25
Christophe Lemercier <lemercier60@gmail.com>
2020-07-22 20:31:36 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const connectDB = require('./db/connect')
 const dotenv = require('dotenv');
 const cors = require('cors');
+const memberRouter = require('./routers/member')
 
 dotenv.config();
-
-const connectDB = require('./db/connect')
-
+// Connect to Database
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+
+// allow CORS
+app.use(cors());
+
+// uri & router used
+app.use('/api/members', memberRouter)
 
 module.exports = app;
 
